03cd9b9faeae6de9b042870fdcb42af6336754dd
Henko <hrabie@retrorabbit.co.za>
2017-07-09 15:35:19 +0200

    Added Empty Signin Controller

---
+++ ProjectR/Controllers/signInController.swift
@@ -0,0 +1,17 @@
+//
+//  signInController.swift
+//  ProjectR
+//
+//  Created by Henko on 2017/07/09.
+//  Copyright © 2017 Retro Rabbit Professional Services. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+//import GoogleSignIn
+
+class signInController: UIViewController {
+    
+}
