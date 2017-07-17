7fd0ee53ac6bdd2869526169ab2a8ca28b9ad66a
Wesley Buck <wbuck@retrorabbit.co.za>
2017-07-17 18:56:35 +0200

    Added TabNavigationController

---
+++ ProjectR/TabNavigationController.swift
@@ -0,0 +1,81 @@
+//
+//  TabNavigationController.swift
+//  ProjectR
+//
+//  Created by Wesley Buck on 2017/07/17.
+//  Copyright © 2017 Retro Rabbit Professional Services. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class TabNavigationController: Material.BottomNavigationController {
+    override init() {
+        super.init(viewControllers: [
+            ToolNavigationController(rootViewController: FeedNavigationController.instance),
+            ToolNavigationController(rootViewController: NearbyNavigationControllerX.instance),
+            ToolNavigationController(rootViewController: ChatNavigationControllerX.instance),
+            ToolNavigationController(rootViewController: IntroNavigationController.instance)
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    open override func prepare() {
+        super.prepare()
+        prepareTabBar()
+        delegate = self
+    }
+    
+    fileprivate func prepareTabBar() {
+        tabBar.backgroundColor = Color.blueGrey.darken4
+        
+        tabBar.selectionIndicatorImage =
+            imageWithColor(
+                Material.Color.blueGrey.base,
+                size: CGSize(
+                    width: tabBar.frame.width / 4.0,
+                    height: tabBar.frame.height))
+        
+        let tabBarItemApperance = UITabBarItem.appearance()
+        tabBarItemApperance.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.white], for: .normal)
+        tabBarItemApperance.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.white], for: .selected)
+    }
+    
+    func imageWithColor(_ color: UIColor, size: CGSize) -> UIImage {
+        let rect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+// Keep whisper on all tabs
+
+extension TabNavigationController {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if let currentMessage = Whisper.getCurrentMessage() {
+            Whisper.whisper(currentMessage, action: .present)
+            if currentMessage.duration != .infinite {
+                ThreadService.delay(seconds: currentMessage.duration.rawValue) {
+                    UIViewController.hideWhisper()
+                }
+            }
+        } else {
+            Whisper.silence()
+        }
+    }
+}
+
+enum NavigationTab: Int {
+    case kalido = 0
+    case nearby = 1
+    case chat = 2
+    case introduce = 3
+}
+
