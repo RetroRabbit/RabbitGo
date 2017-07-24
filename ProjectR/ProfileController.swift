edd1c2f471a2985a946b0112b73c3b9a16615b4f
Janeen Bredenkamp <jbredenkamp@retrorabbit.co.za>
2017-07-24 20:44:22 +0200

    Added scrollView and profile image placeholder

--- ProjectR/ProfileController.swift
+++ ProjectR/ProfileController.swift
@@ -10,26 +10,30 @@ import Foundation
 import UIKit
 import Material
 
-class ProfileController: UIViewController {
+class ProfileController: UIViewController, UIScrollViewDelegate {
+    fileprivate let scrollView = UIScrollView(forAutoLayout: ())
+    
     static let instance = ProfileController()
     
+    fileprivate var imgProfilePlaceholder: UIImageView
+    /*
     let titleLabel: UILabel = {
         let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
+//        lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.attributedText = NSAttributedString(string: "Project R", attributes: Style.extra_large_blue_grey)
         return lbl
     }()
     
     let nameLabel: UILabel = {
         let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
+//        lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.attributedText = NSAttributedString(string: "Name Surname", attributes: Style.extra_large_blue_grey)
         return lbl
-    }()
+    }()*/
     
     let subTitleLabel: UILabel = {
         let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
+//        lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.attributedText = NSAttributedString(string: "- RABBITEER -", attributes: Style.extra_large_blue_grey)
         return lbl
     }()
@@ -68,31 +72,29 @@ class ProfileController: UIViewController {
         return entry
     }()
     
-    lazy var editButton:Button = {
-        let btn = Button()
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        //        btn.setAttributedTitle(NSAttributedString(string: "NEXT", attributes: Style.heading3), for: .normal)
+    lazy var editButton:ProjectRButton = {
+        let btn = ProjectRButton()
         btn.setTitle("EDIT", for: .normal)
-        btn.backgroundColor = UIColor.retroBlack
-        btn.layer.cornerRadius = 2
-        btn.layer.borderWidth = 2
-        btn.titleLabel?.textColor = UIColor.retroGreen
-        btn.tintColor = UIColor.retroGreen
         btn.addTarget(self, action: #selector(SignInController.onNext), for: UIControlEvents.touchUpInside)
         return btn
     }()
     
     init() {
+        imgProfilePlaceholder = UIImageView(image: UIImage(named: "image_placeholder"))
+        imgProfilePlaceholder.contentMode = .scaleAspectFit
+        imgProfilePlaceholder.clipsToBounds = true
+        
         super.init(nibName: nil, bundle: nil)
-        //super.init()
-        //super.init(hiding: NavigationHide.toBottom)
+//        super.init()
+//        super.init(hidd)
+//        super.init(hiding: NavigationHide.toBottom)
         navigationController?.title = "Leaderboard Position #43"
         tabBarItem.title = "My Profile"
         tabBarItem.image = Material.Icon.pen
     }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
@@ -100,29 +102,37 @@ class ProfileController: UIViewController {
     
         view.backgroundColor = Style.color.grey_dark
         
-        view.addSubview(titleLabel)
-        view.addSubview(nameLabel)
-        view.addSubview(subTitleLabel)
-        view.addSubview(nameEntry)
-        view.addSubview(emailEntry)
-        view.addSubview(universityEntry)
-        view.addSubview(degreeEntry)
-        view.addSubview(yearEntry)
-        view.addSubview(editButton)
+        view.addSubview(scrollView)
+        scrollView.delegate = self
+        
+//        scrollView.addSubview(titleLabel)
+//        scrollView.addSubview(nameLabel)
+        scrollView.addSubview(subTitleLabel)
+        scrollView.addSubview(imgProfilePlaceholder)
+        scrollView.addSubview(nameEntry)
+        scrollView.addSubview(emailEntry)
+        scrollView.addSubview(universityEntry)
+        scrollView.addSubview(degreeEntry)
+        scrollView.addSubview(yearEntry)
+        scrollView.addSubview(editButton)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        scrollView.autoPinEdgesToSuperviewEdges()
+        
         //Title Section:
-        titleLabel.frame = CGRect(x: (Screen.width - titleLabel.intrinsicContentSize.width)/2, y: 20, width: titleLabel.intrinsicContentSize.width, height: titleLabel.intrinsicContentSize.height)
+//        titleLabel.frame = CGRect(x: (Screen.width - titleLabel.intrinsicContentSize.width)/2, y: 20, width: titleLabel.intrinsicContentSize.width, height: titleLabel.intrinsicContentSize.height)
         
-        nameLabel.frame = CGRect(x: (Screen.width - nameLabel.intrinsicContentSize.width)/2, y: titleLabel.frame.bottom + 10, width: nameLabel.intrinsicContentSize.width, height: nameLabel.intrinsicContentSize.height)
+//        nameLabel.frame = CGRect(x: (Screen.width - nameLabel.intrinsicContentSize.width)/2, y: titleLabel.frame.bottom + 10, width: nameLabel.intrinsicContentSize.width, height: nameLabel.intrinsicContentSize.height)
         
-        subTitleLabel.frame = CGRect(x: (Screen.width - subTitleLabel.intrinsicContentSize.width)/2, y: nameLabel.frame.bottom + 5, width: subTitleLabel.intrinsicContentSize.width, height: subTitleLabel.intrinsicContentSize.height)
+//        subTitleLabel.frame = CGRect(x: (Screen.width - subTitleLabel.intrinsicContentSize.width)/2, y: nameLabel.frame.bottom + 5, width: subTitleLabel.intrinsicContentSize.width, height: subTitleLabel.intrinsicContentSize.height)
+        
+        imgProfilePlaceholder.frame = CGRect(x: Screen.width/2 - imgProfilePlaceholder.intrinsicContentSize.width/2, y: 20, width: imgProfilePlaceholder.frame.width, height: imgProfilePlaceholder.frame.height)
         
         //Entries Section:
-        nameEntry.frame = CGRect(x: 40, y: subTitleLabel.frame.bottom + 40, width: Screen.width - 80, height: 40)
+        nameEntry.frame = CGRect(x: 40, y: imgProfilePlaceholder.frame.bottom + 40, width: Screen.width - 80, height: 40)
         
         emailEntry.frame = CGRect(x: 40, y: nameEntry.frame.bottom + 40, width: Screen.width - 80, height: 40)
         
@@ -133,6 +143,8 @@ class ProfileController: UIViewController {
         yearEntry.frame = CGRect(x: 40, y: degreeEntry.frame.bottom + 40, width: Screen.width - 80, height: 40)
         
         //edit button:
-        editButton.frame = CGRect(x: 40, y: yearEntry.frame.bottom + 40, width: Screen.width - 80, height: 40)
+        editButton.frame = CGRect(x: 40, y: yearEntry.frame.bottom + 40, width: Screen.width - 80, height: Style.button_height)
+        
+        scrollView.contentSize = CGSize(width: Screen.width, height: editButton.frame.bottom)
     }
 }
